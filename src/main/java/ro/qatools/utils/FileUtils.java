1934655fd4ab1929b97ccc477db2538594d90bd4
danrusu <danginkgo@yahoo.com>
2023-06-30 19:11:45 +0300

    fix: cleanup JSON creation; use Map.of

--- src/main/java/ro/qatools/utils/FileUtils.java
+++ src/main/java/ro/qatools/utils/FileUtils.java
@@ -1,7 +1,5 @@
 package ro.qatools.utils;
 
-import org.json.simple.JSONObject;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +7,8 @@ import java.nio.file.Path;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class FileUtils {
-    public static void saveToDisk(Path path, JSONObject json) throws IOException {
-        Files.write(path, json.toString().replace("\\", "").getBytes(UTF_8));
+    public static void saveToDisk(Path path, String json) throws IOException {
+        Files.write(path, json.getBytes(UTF_8));
     }
 
 }
--- src/test/java/ro/qatools/FirstTest.java
+++ src/test/java/ro/qatools/FirstTest.java
@@ -1,13 +1,17 @@
 package ro.qatools;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import ro.qatools.pages.SeleniumGithubPage;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Map;
 
+import static java.lang.Integer.parseInt;
+import static org.json.simple.JSONObject.toJSONString;
 import static ro.qatools.utils.FileUtils.saveToDisk;
 
 public class FirstTest extends BaseTest {
@@ -20,9 +24,10 @@ public class FirstTest extends BaseTest {
         var cloneUrl = seleniumGithubPage.getCloneUrl();
         var releasesCount = seleniumGithubPage.getReleasesCount();
 
-        JSONObject jsonInfo = new JSONObject();
-        jsonInfo.put("cloneUrl", cloneUrl);
-        jsonInfo.put("releasesCount", Integer.parseInt(releasesCount));
+        String jsonInfo = toJSONString(Map.of(
+                "cloneUrl", cloneUrl,
+                "releasesCount", parseInt(releasesCount))
+        ).replace("\\", "");
 
         saveToDisk(
                 Path.of(
